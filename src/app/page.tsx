f8fb7a55dc1422aee38b2714a47672e600d18662
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-12-18 15:07:42 +0400

    starting project and implement database and models

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center">
+      <Button>Click me</Button>
+    </div>
+  );
+}
