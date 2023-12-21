ed946312ced96d3ac41740d66ea8b5325df764ad
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-12-21 12:37:17 +0400

    validation errors

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,8 +1,39 @@
 "use server";
+import { z } from "zod";
 
-export async function createTopic() {
+const createTopicShcema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, {
+      message: "Must be lowercase letters or dashes without space",
+    }),
+  description: z.string().min(10),
+});
 
+interface CreateTopicFormState {
+  errors: {
+    name?: String[];
+    description?: String[];
+    _form?: String[];
+  };
+}
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = createTopicShcema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
 
-
-    //Todo: revalidate the homepage
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+  return {
+    errors: {},
+  };
 }
